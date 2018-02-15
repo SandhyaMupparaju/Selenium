fccc839cf309cf916cc2efbfa97c437d47580ebb
Sandhya Mupparaju <Sandhya Mupparaju@192.168.0.19>
2018-02-14 19:26:42 -0500

    Code changes in existing class and added new class"test2"

--- Git_Sample/src/test/Testing.java
+++ Git_Sample/src/test/Testing.java
@@ -3,5 +3,7 @@ package test;
 public class Testing {
 	public static void main(String[] args) {
 		System.out.println("Tesitng");
+		System.out.println("Tesitng1");
+		
 	}
 }
---
+++ Git_Sample/src/test/test2.java
@@ -0,0 +1,8 @@
+package test;
+
+public class test2 {
+	public void add() {
+		System.out.println("test");
+
+	}
+}
